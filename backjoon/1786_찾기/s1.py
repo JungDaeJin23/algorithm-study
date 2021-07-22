a32d4779788a4ece44c21b8c9453b3e4aa078e76
JeongDaeJin23 <12jund000@naver.com>
2021-07-22 22:13:36 +0900

    문자가 중복되는 문제 해결 잘 하자

---
+++ backjoon/1786_찾기/s1.py
@@ -0,0 +1,7 @@
+T = input()
+P = input()
+# 전처리
+
+idx = 0
+while idx < len(T):
+    idx += 1
--- backjoon/21609_상어_중학교/s3.py
+++ backjoon/21609_상어_중학교/s3.py
@@ -1,12 +1,94 @@
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
-ans = 0
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
+ans = 0
 
-def BFS(row, col, block):
-    global board, N, counted
 
+def BFS(queue, block):
+    global dr, dc, board, N, counted
+    cnt = 0
+    magic_block_cnt = 0
+    while queue:
+        row, col = queue.pop(0)
+        cnt += 1
+        if board[row][col] == 0:
+            magic_block_cnt += 1
+        for d in range(4):
+            nr = row + dr[d]
+            nc = col + dc[d]
+            if 0 <= nr < N and 0 <= nc < N and not counted[nr][nc] and (board[nr][nc] == block or board[nr][nc] == 0):
+                queue.append((nr, nc))
+                counted[nr][nc] = True
+    return cnt, magic_block_cnt
+
+
+def find_biggest_blocks():
+    global board, N, counted, criterion
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] > 0 and not counted[i][j]:
+                counted[i][j] = True
+                cnt, magic_block_cnt = BFS([(i, j)], board[i][j])
+                if cnt > criterion[0]:
+                    criterion = (cnt, magic_block_cnt, i, j)
+                elif cnt > 1 and cnt == criterion[0]:
+                    if magic_block_cnt > criterion[1]:
+                        criterion = (cnt, magic_block_cnt, i, j)
+                    elif magic_block_cnt == criterion[1]:
+                        if i > criterion[2]:
+                            criterion = (cnt, magic_block_cnt, i, j)
+                        elif i == criterion[2]:
+                            if j > criterion[3]:
+                                criterion = (cnt, magic_block_cnt, i, j)
+                # 무지개 블록 초기화
+                if magic_block_cnt > 0:
+                    for r in range(N):
+                        for c in range(N):
+                            if board[r][c] == 0:
+                                counted[r][c] = False
+
+
+def delete_blocks(queue, block):
+    global dr, dc, board, N
+    board[queue[0][0]][queue[0][1]] = -2
+    while queue:
+        row, col = queue.pop(0)
+        for d in range(4):
+            nr = row + dr[d]
+            nc = col + dc[d]
+            if 0 <= nr < N and 0 <= nc < N and (board[nr][nc] == block or board[nr][nc] == 0):
+                queue.append((nr, nc))
+                board[nr][nc] = -2
+
+
+def gravity():
+    global board
+    i = N-2
+    while i >= 0:
+        for j in range(N):
+            if board[i][j] >= 0:
+                down_i = i+1
+                while down_i < N and board[down_i][j] == -2:
+                    down_i += 1
+                down_i -= 1
+                board[i][j], board[down_i][j] = board[down_i][j], board[i][j]
+        i -= 1
+
+
+def turn_anti_clockwise():
+    global N, board
+    new_board = []
+    j = N-1
+    while j >= 0:
+        tmp = []
+        i = 0
+        while i < N:
+            tmp.append(board[i][j])
+            i += 1
+        new_board.append(tmp)
+        j -= 1
+    return new_board
 
 
 while True:

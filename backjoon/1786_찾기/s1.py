16578f0c32477b4f3198c7ef5d7207d3956bddf4
JeongDaeJin23 <12jund000@naver.com>
2021-07-29 20:21:22 +0900

    impossible

--- backjoon/1786_찾기/s1.py
+++ backjoon/1786_찾기/s1.py
@@ -1,7 +1,34 @@
 T = input()
 P = input()
+return_position = [0] * len(P)
+cnt = 0
+ans = []
 # 전처리
-
-idx = 0
-while idx < len(T):
+idx = 1
+while idx < len(P):
+    jdx = 0
+    while idx < len(P) and P[idx] == P[jdx]:
+        return_position[idx] = jdx + 1
+        idx += 1
+        jdx += 1
     idx += 1
+print(return_position)
+idx = 0
+jdx = 0
+while idx < len(T) - len(P) + 1:
+    while jdx < len(P):
+        if T[idx + jdx] != P[jdx]:
+            if jdx > 0:
+                jdx -= 1
+            idx += jdx - return_position[jdx]
+            jdx = tmp
+            break
+        jdx += 1
+    else:
+        jdx = 0
+        cnt += 1
+        ans.append(idx+1)
+    idx += 1
+
+print(cnt)
+print(*ans)

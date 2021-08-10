2f54be50db7897d306d05a400ee5f2228ecb87a8
JeongDaeJin23 <12jund000@naver.com>
2021-08-10 21:21:46 +0900

    normal

---
+++ backjoon/17143_낚시왕/s2.py
@@ -0,0 +1,79 @@
+R, C, M = map(int, input().split())
+fishing_hole = [[0]*C for _ in range(R)]
+sharks = []
+divide_r = 2 * R - 2
+divide_c = 2 * C - 2
+for idx in range(M):
+    # (r, c), s = speed, d = direction, z = size
+    #      위,아래,오른쪽,왼쪽
+    # d = [1, 2, 3, 4]
+    # 두 상어가 같은 크기를 갖는 경우는 없고, 하나의 칸에 둘 이상의 상어가 있는 경우는 없다.
+    r, c, s, d, z = map(int, input().split())
+    if d == 1 or d == 2:
+        s %= divide_r
+    else:
+        s %= divide_c
+    sharks.append((r-1, c-1, s, d, z))
+    fishing_hole[r-1][c-1] = (z, idx)
+dr = [0, -1, 1, 0, 0]
+dc = [0, 0, 0, 1, -1]
+# fishing
+king_of_fishing = 0
+# moving
+for col in range(C):
+    # fishing
+    for row in range(R):
+        if fishing_hole[row][col]:
+            king_of_fishing += fishing_hole[row][col][0]
+            sharks[fishing_hole[row][col][1]] = 0
+            fishing_hole[row][col] = 0
+            break
+    # shark moves
+    sharks_old_one = sharks[:]
+    for idx in range(M):
+        if sharks[idx] == 0:
+            continue
+        r, c, s, d, z = sharks[idx]
+        # s
+        for _ in range(s):
+            nr = r + dr[d]
+            nc = c + dc[d]
+            if nr < 0 or nr >= R or nc < 0 or nc >= C:
+                if d == 1:
+                    d = 2
+                elif d == 2:
+                    d = 1
+                elif d == 3:
+                    d = 4
+                else:
+                    d = 3
+                nr = r + dr[d]
+                nc = c + dc[d]
+            r = nr
+            c = nc
+        sharks[idx] = (r, c, s, d, z)
+
+    # shark eat
+    for idx in range(M):
+        if sharks_old_one[idx] == 0:
+            continue
+        r, c, s, d, z = sharks_old_one[idx]
+        fishing_hole[r][c] = 0
+    tmp = []
+    for idx in range(M):
+        if sharks[idx] == 0:
+            continue
+        r, c, s, d, z = sharks[idx]
+        if fishing_hole[r][c]:
+            if fishing_hole[r][c][0] > z:
+                tmp.append(idx)
+            else:
+                tmp.append(fishing_hole[r][c][1])
+                fishing_hole[r][c] = (z, idx)
+        else:
+            fishing_hole[r][c] = (z, idx)
+    for idx in tmp:
+        sharks[idx] = 0
+print(king_of_fishing)
+
+

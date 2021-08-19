a4ce0f6eb872eca85b8180498c1d1b88a22aa9cd
JeongDaeJin23 <12jund000@naver.com>
2021-08-20 00:25:49 +0900

    hard

--- backjoon/18352_특정거리도시찾기/s1.py
+++ backjoon/18352_특정거리도시찾기/s1.py
@@ -1,18 +1,36 @@
+import sys
+input = sys.stdin.readline
+
+
 N, M, K, X = map(int, input().split())
 graph = [[] for _ in range(N+1)]
 cost_arr = [987654321] * (N + 1)
 visited = [0] * (N + 1)
+ans = []
+visited[0] = 1
 for _ in range(M):
     s, g = map(int, input().split())
     graph[s].append(g)
 
+visited[X] = 1
 cost_arr[X] = 0
-while sum(visited) < N:
-    min_idx = 0
-    min_cost = 987654321
-    for idx in range(len(cost_arr)):
-        if not visited[idx] and cost_arr[idx] < min_cost:
-            min_idx = idx
-            min_cost = cost_arr[idx]
+for _ in range(N):
+    for node in graph[X]:
+        if cost_arr[X] + 1 < cost_arr[node]:
+            cost_arr[node] = cost_arr[X] + 1
+    X = 0
+    for node in range(1, N+1):
+        if not visited[node] and cost_arr[node] < cost_arr[X]:
+            X = node
+    visited[X] = 1
+    if cost_arr[X] == K:
+        break
 
-    visited[min_idx] = 1
+for idx in range(1, len(cost_arr)):
+    if cost_arr[idx] == K:
+        ans.append(idx)
+if len(ans):
+    print(*ans, sep='\n')
+else:
+    print(-1)
+
